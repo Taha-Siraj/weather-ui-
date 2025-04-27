48c9466fb47adc1fb160086cee889f7649692372
tahasiraj101 <pikachugaming899@gmail.com>
2025-04-27 12:31:41 -0700

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     if (condition.includes('sunny')) return 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e';
     if (condition.includes('rain')) return 'https://images.unsplash.com/photo-1515694346937-94d85e41e6f0';
     if (condition.includes('cloud')) return 'https://images.unsplash.com/photo-1499346030926-9a72daac6c63';
-    return 'https://images.unsplash.com/photo-1592210454359-9043f067919b';e
+    return 'https://images.unsplash.com/photo-1592210454359-9043f067919b';
   };
 
   const checkWeather = () => {
@@ -75,21 +75,21 @@ const App = () => {
             type="text"
             placeholder="Enter City Name"
             onChange={(e) => setCity(e.target.value)}
-            className="w-full py-3 outline-0 text-black text-xl px-4 bg-white/10 text-white placeholder-gray-300 border border-white/30 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+            className="w-full font-mono py-3 outline-0 text-xl px-4 bg-white/10 placeholder-black border border-white/30 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition text-black "
           />
         </div>
         <button
           onClick={checkWeather}
-          className="w-full py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold rounded-lg hover:from-blue-600 hover:to-purple-700 transition-all duration-300"
+          className="w-full py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold rounded-lg hover:from-blue-600 hover:to-purple-700 transition-all duration-300 outline-none"
         >
           Search Weather
         </button>
         {weather.cityname && (
-          <div className="mt-6 text-white space-y-2 text-black capitalize">
+          <div className="mt-6 font-mono space-y-2 text-black capitalize">
             <h2 className="text-2xl font-semibold ">{weather.cityname}, {weather.country}</h2>
-            <p className="text-lg">Temperature: {weather?.temperature?.current} {weather?.temperature?.unit}</p>
-            <p className="text-lg">Feels Like: {weather?.temperature?.feels_like} {weather?.temperature?.unit}</p>
-            <p className="text-lg">Humidity: {weather?.humidity}%</p>
+            <p className="text-lg">Temperature: {weather?.temperature?.current}{weather?.temperature?.unit}</p>
+            <p className="text-lg">Feels Like: {weather?.temperature?.feels_like}{weather?.temperature?.unit}</p>
+            <p className="text-lg">Humidity: {weather?.humidity}</p>
             <p className="text-lg">Wind Speed: {weather?.wind?.speed} km/h</p>
             {weather.condition && (
               <p className="text-lg capitalize">Condition: {weather.condition}</p>
